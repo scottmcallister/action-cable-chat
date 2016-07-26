296167263ede2864b6f77e3e2b57a29464c5455d
Scott McAllister <scottm@nextopia.com>
2016-07-26 11:14:00 +0000

    fix broadcasting logic in job

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,26 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    // Called when there's incoming data on the websocket for this channel
+    document.getElementById("messages").innerHTML += data['message'];
+  },
+
+  speak: function(message) {
+    return this.perform('speak', { message: message });
+  }
+});
+
+$(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+  if (event.keyCode === 13) {
+    App.room.speak(event.target.value);
+    event.target.value = '';
+    return event.preventDefault();
+  }
+});
